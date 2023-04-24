c211f8c312dfa6e4634d83ff61424a34f28b97f4
Aman Bagrecha <bagrecha.aman588@gmail.com>
2023-04-24 22:40:34 +0530

    update h5 to include multi file

--- h5_to_geotif.py
+++ h5_to_geotif.py
@@ -2,55 +2,79 @@
 # source: https://blackmarble.gsfc.nasa.gov/tools/OpenHDF5.py
 from osgeo import gdal
 import glob
-## List input raster files
+import os
+from pathlib import Path
 
-rasterFiles = glob.glob("*.h5")
-#print(rasterFiles)
 
-#Get File Name Prefix
-rasterFilePre = rasterFiles[0][:-3]
-print(rasterFilePre)
+def convert_h5_to_tif(rasterFile):
 
-fileExtension = "_BBOX.tif"
+    # Get File Name Prefix
+    rasterFilePre = Path(rasterFile).stem
 
-## Open HDF file
-hdflayer = gdal.Open(rasterFiles[0], gdal.GA_ReadOnly)
+    print(rasterFilePre)
 
-#print (hdflayer.GetSubDatasets())
+    fileExtension = "_BBOX.tif"
 
-# Open raster layer
-#hdflayer.GetSubDatasets()[0][0] - for first layer
-#hdflayer.GetSubDatasets()[1][0] - for second layer ...etc
-subhdflayer = hdflayer.GetSubDatasets()[0][0]
-rlayer = gdal.Open(subhdflayer, gdal.GA_ReadOnly)
-#outputName = rlayer.GetMetadata_Dict()['long_name']
+    ## Open HDF file
+    hdflayer = gdal.Open(rasterFile, gdal.GA_ReadOnly)
 
-#Subset the Long Name
-outputName = subhdflayer[92:]
+    subdatasets = hdflayer.GetSubDatasets()
 
-outputNameNoSpace = outputName.strip().replace(" ","_").replace("/","_")
-outputNameFinal = outputNameNoSpace + rasterFilePre + fileExtension
-print(outputNameFinal)
+    for subdataset, meta in subdatasets:
 
-outputFolder = "./"
+        if "Gap_Filled_DNB_BRDF-Corrected_NTL" in subdataset:
+            subhdflayer = subdataset
 
-outputRaster = outputFolder + outputNameFinal
+    if not subhdflayer:
+        raise Exception("did not find gap filled dnb NTL")
 
-#collect bounding box coordinates
-HorizontalTileNumber = int(rlayer.GetMetadata_Dict()["HorizontalTileNumber"])
-VerticalTileNumber = int(rlayer.GetMetadata_Dict()["VerticalTileNumber"])
-    
-WestBoundCoord = (10*HorizontalTileNumber) - 180
-NorthBoundCoord = 90-(10*VerticalTileNumber)
-EastBoundCoord = WestBoundCoord + 10
-SouthBoundCoord = NorthBoundCoord - 10
+    # Open raster layer
+    rlayer = gdal.Open(subhdflayer, gdal.GA_ReadOnly)
 
-EPSG = "-a_srs EPSG:4326" #WGS84
+    # Subset the Long Name
+    outputName = Path(subhdflayer.split(":")[-1]).stem
 
-translateOptionText = EPSG+" -a_ullr " + str(WestBoundCoord) + " " + str(NorthBoundCoord) + " " + str(EastBoundCoord) + " " + str(SouthBoundCoord)
+    outputNameFinal = f"{outputName}-{rasterFilePre}{fileExtension}"
+    print(outputNameFinal)
 
-translateoptions = gdal.TranslateOptions(gdal.ParseCommandLine(translateOptionText))
-gdal.Translate(outputRaster,rlayer, options=translateoptions)
+    outputFolder = "./output"
+    if not os.path.exists(outputFolder):
+        os.makedirs(outputFolder, exist_ok=True)
 
-#Display image in QGIS (run it within QGIS python Console) - remove comment to display
-#iface.addRasterLayer(outputRaster, outputNameFinal)
+    outputRaster = os.path.join(outputFolder, outputNameFinal)
+
+    # collect bounding box coordinates
+    HorizontalTileNumber = int(rlayer.GetMetadata_Dict()["HorizontalTileNumber"])
+    VerticalTileNumber = int(rlayer.GetMetadata_Dict()["VerticalTileNumber"])
+
+    WestBoundCoord = (10 * HorizontalTileNumber) - 180
+    NorthBoundCoord = 90 - (10 * VerticalTileNumber)
+    EastBoundCoord = WestBoundCoord + 10
+    SouthBoundCoord = NorthBoundCoord - 10
+
+    EPSG = "-a_srs EPSG:4326"  # WGS84
+
+    translateOptionText = (
+        EPSG
+        + " -a_ullr "
+        + str(WestBoundCoord)
+        + " "
+        + str(NorthBoundCoord)
+        + " "
+        + str(EastBoundCoord)
+        + " "
+        + str(SouthBoundCoord)
+    )
+
+    translateoptions = gdal.TranslateOptions(gdal.ParseCommandLine(translateOptionText))
+    gdal.Translate(outputRaster, rlayer, options=translateoptions)
+
+    print("Done")
+
+
+if __name__ == "__main__":
+
+    ## List input raster files
+    rasterFiles = glob.glob("*.h5")
+    for rasterFile in rasterFiles:
+        convert_h5_to_tif(rasterFile)
